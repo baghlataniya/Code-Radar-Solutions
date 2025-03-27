7f0dde93a12911a9990d450158bb468da93391ce
baghlataniya <baghlatanya550@gmail.com>
2025-03-27 11:59:08 +0530

    Solution to Print a Pyramid Pattern

--- Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
+++ Print a Pyramid Pattern@@67877e1818d53eb724f435bf/code.c
@@ -1,18 +1,15 @@
-// Your code here...
 #include <stdio.h>
-
-int main() {
+int main(){
     int n;
-    scanf("%d", &n); // Read the number of rows
-
-    for (int i = 1; i <= n; i++) {  // Loop for each row
-        for (int j = 1; j <= n - i; j++) {  
-            printf(" ");  // Print spaces for alignment
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
         }
-        for (int k = 1; k <= (2 * i - 1); k++) {  
-            printf("*");  // Print stars
+        for(int k=1;k<=2*n-1;k++){
+            printf("* ");
         }
-        printf("\n");  // Move to next line
+        printf("\n");
     }
 
     return 0;
