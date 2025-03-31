6f4eed1f5ccaf6e131e027ea2b163d7f768b38fc
baghlataniya <baghlatanya550@gmail.com>
2025-04-01 04:02:17 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,44 +1,32 @@
 #include <stdio.h>
-
-// Function to reverse a portion of the array
-void reverse(int arr[], int start, int end) {
-    while (start < end) {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
+void reverse(int arr[],int start,int end){
+    while(start<end){
+        int temp=arr[start];
+        arr[start]=arr[end];
+        temp=arr[end];
         start++;
         end--;
+
     }
 }
-
-int main() {
-    int N, K;
-    scanf("%d", &N); // Read size of array
-    int arr[N];
-
-    // Read the array elements
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &arr[i]);
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int k;
+    scanf("%d",&k)
+    if(k>n) k=k%10;
+    reverse(arr,0,n-1);
+    reverse(arr,0,k-1);
+    reverse(arr,k,n-1);
+    for(int i=0;i<n;i++){
+        print("%d",arr[i]);
     }
 
-    scanf("%d", &K); // Read number of rotations
-    K = K % N; // Handle cases where K > N
-
-    // Reverse the entire array
-    reverse(arr, 0, N - 1);
-    // Reverse first K elements
-    reverse(arr, 0, K - 1);
-    // Reverse remaining elements
-    reverse(arr, K, N - 1);
-
-    // Print rotated array
-    for (int i = 0; i < N; i++) {
-        printf("%d\n", arr[i]);
-    }
-    
     return 0;
 }
-
-
 
 
