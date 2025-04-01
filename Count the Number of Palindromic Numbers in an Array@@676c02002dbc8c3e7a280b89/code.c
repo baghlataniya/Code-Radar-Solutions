6efdc02336dc4feb60d1002adf16e24555909a93
baghlataniya <baghlatanya550@gmail.com>
2025-04-01 12:03:20 +0530

    Solution to Count the Number of Palindromic Numbers in an Array

--- Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
+++ Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
@@ -2,6 +2,11 @@
 
 // Function to check if a number is a palindrome
 int isPalindrome(int num) {
+    // Negative numbers are not considered palindromes
+    if (num < 0) {
+        return 0;
+    }
+    
     int original = num, reversed = 0, remainder;
     
     // Reverse the number
@@ -23,19 +28,3 @@ int main() {
     int count = 0;
     
     // Input the array elements
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    
-    // Count how many numbers are palindromes
-    for (int i = 0; i < n; i++) {
-        if (isPalindrome(arr[i])) {
-            count++;
-        }
-    }
-    
-    // Output the count of palindromes
-    printf("%d\n", count);
-    
-    return 0;
-}
