34a69234f9237e01f604d06f10c97e8ad4822c0a
baghlataniya <baghlatanya550@gmail.com>
2025-04-01 12:02:10 +0530

    Solution to Count the Number of Palindromic Numbers in an Array

--- Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
+++ Count the Number of Palindromic Numbers in an Array@@676c02002dbc8c3e7a280b89/code.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
-int count(int arr[],int n){
-    int cnt=0;
-    for(int i=0;i<n/2;i++){
-        if(arr[i]==arr[n-i-1]){
-            cnt++;
+
+// Function to check if a number is a palindrome
+int isPalindrome(int num) {
+    int original = num, reversed = 0, remainder;
+    
+    // Reverse the number
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num /= 10;
+    }
+    
+    // Check if the original number is equal to its reverse
+    return original == reversed;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);  // Input the number of elements
+    
+    int arr[n];
+    int count = 0;
+    
+    // Input the array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    // Count how many numbers are palindromes
+    for (int i = 0; i < n; i++) {
+        if (isPalindrome(arr[i])) {
+            count++;
         }
     }
-    return cnt;
-
-}
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
-    }
-    int x=count(arr,n);
-    printf("%d",x);
+    
+    // Output the count of palindromes
+    printf("%d\n", count);
+    
+    return 0;
 }
